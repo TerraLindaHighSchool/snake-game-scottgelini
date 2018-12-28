4839960b64899ff3711af27dffb14cfb7f953434
Bruce Gustin <35908618+bgustin53@users.noreply.github.com>
2018-12-27 16:23:45 -0800

    Update SnakeGame.java

--- app/src/main/java/com/example/bruce/snake_startercode/SnakeGame.java
+++ app/src/main/java/com/example/bruce/snake_startercode/SnakeGame.java
@@ -1,4 +1,6 @@
 package com.example.bruce.snake_startercode;
 
 public class SnakeGame {
+  public SnakeGame(int beginningDirection, int beginningSpriteDim, int beginningX, int beginningY, int width, int height){
+  }
 }
