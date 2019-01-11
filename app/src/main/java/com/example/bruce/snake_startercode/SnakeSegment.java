be7c1acf08688617650a6c3620feca3bec592fe2
sg00251 <sg00251@srcs.org>
2019-01-11 14:29:13 -0800

    snake

--- app/src/main/java/com/example/bruce/snake_startercode/SnakeSegment.java
+++ app/src/main/java/com/example/bruce/snake_startercode/SnakeSegment.java
@@ -1,4 +1,46 @@
 package com.example.bruce.snake_startercode;
 
 public class SnakeSegment {
+    private int mXLoc;
+    private int mYLoc;
+    private int mLengthOfSnake;
+    private Body mBodyParts;
+    private int mDegree;
+
+    public enum Body {
+
+        HEAD, BODY, TAIL;
+    }
+
+    /*************************************************
+     * Getters and Setters
+     **************************************************/
+
+    public int getmXLoc() {
+        return mXLoc;
+    }
+
+    public int getmYLoc() {
+        return mYLoc;
+    }
+
+    public int getmLengthOfSnake() {
+        return mLengthOfSnake;
+    }
+
+    public void setmLengthOfSnake(int mLengthOfSnake) {
+        this.mLengthOfSnake = mLengthOfSnake;
+    }
+
+    public Body getmBodyParts() {
+        return mBodyParts;
+    }
+
+    public int getmDegree() {
+        return mDegree;
+    }
+
+    public void setmDegree(int mDegree) {
+        this.mDegree = mDegree;
+    }
 }
---
+++ app/src/test/java/com/example/bruce/snake_startercode/SnakeSegmentTest.java
@@ -0,0 +1,36 @@
+package com.example.bruce.snake_startercode;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SnakeSegmentTest {
+
+    @Test
+    public void getmXLoc() {
+    }
+
+    @Test
+    public void getmYLoc() {
+    }
+
+    @Test
+    public void getmLengthOfSnake() {
+    }
+
+    @Test
+    public void setmLengthOfSnake() {
+    }
+
+    @Test
+    public void getmBodyParts() {
+    }
+
+    @Test
+    public void getmDegree() {
+    }
+
+    @Test
+    public void setmDegree() {
+    }
+}
